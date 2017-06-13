e8ad41b337318747d0fa9a5e396dcf89fb520d4b
Nathan Malkin <nmalkin@cs.berkeley.edu>
2017-06-13 14:29:27 -0700

    Add Spek test framework and tests for phone numbers

---
+++ src/test/kotlin/JsonParsing.kt
@@ -0,0 +1,7 @@
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
+import kotlin.test.assertEquals
+
+class JsonParsingTest : Spek({
+    // TODO
+})
---
+++ src/test/kotlin/PhoneNumberTest.kt
@@ -0,0 +1,36 @@
+import com.beust.jcommander.ParameterException
+import keymonitor.PhoneNumber
+import keymonitor.PhoneNumberValidator
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+
+
+class PhoneNumberTest : Spek({
+    val badNumber = "666"
+    val goodNumber = "+18885550123"
+
+    describe("a phone number") {
+        on("creation") {
+            it("throws an exception if initialized with invalid number") {
+                assertFailsWith(IllegalArgumentException::class, {
+                    PhoneNumber(badNumber)
+                })
+            }
+
+            it("doesn't do that if given a good number") {
+                val number = PhoneNumber(goodNumber)
+                assertNotNull(number)
+            }
+        }
+    }
+
+    describe("the JCommander validator") {
+        it("throws a ParameterException for bad numbers") {
+            assertFailsWith(ParameterException::class, {
+                PhoneNumberValidator().validate("dummy", badNumber)
+            })
+        }
+    }
+})
