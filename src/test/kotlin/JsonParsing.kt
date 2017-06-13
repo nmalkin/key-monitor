5bae621c226726c99864b4bbc287609b8321062e
Nathan Malkin <nmalkin@cs.berkeley.edu>
2017-06-13 16:01:51 -0700

    Add some basic tests for JSON parsing

--- src/test/kotlin/JsonParsing.kt
+++ src/test/kotlin/JsonParsing.kt
@@ -1,7 +1,41 @@
+import keymonitor.PhoneNumber
+import keymonitor.signup.JsonParsingException
+import keymonitor.signup.RegistrationMessage
+import keymonitor.signup.parseJsonFile
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.*
+import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+
+fun assertParsingFails(parsing: () -> Unit) {
+    assertFailsWith(JsonParsingException::class, parsing)
+}
 
 class JsonParsingTest : Spek({
-    // TODO
+    describe("signal-cli JSON output parsing") {
+        on("parsing an entire file") {
+            val goodFile = File(JsonParsingTest::class.java.getResource("good_log.txt")?.toURI())
+            val messages = parseJsonFile(goodFile)
+
+            it("is able to parse it into messages") {
+                assertNotNull(messages)
+            }
+
+            it("gets the right number of messages") {
+                assertEquals(2, messages.size)
+            }
+
+            it("extracts the phone number from a message") {
+                val aMessage: RegistrationMessage = messages.take(1)[0]
+                assertEquals(PhoneNumber("+14155550123"), aMessage.phoneNumber)
+            }
+
+            it("extracts the email from a message") {
+                val aMessage: RegistrationMessage = messages.take(1)[0]
+                assertEquals("test@example.com", aMessage.email)
+            }
+        }
+    }
 })
