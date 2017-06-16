b0e1ad4f4ed47cbd4dfdbaa74ba016cb1436b234
Nathan Malkin <nmalkin@cs.berkeley.edu>
2017-06-16 12:00:31 -0700

    Provide a flexible way of configuring the application

---
+++ src/main/kotlin/keymonitor/common/Config.kt
@@ -0,0 +1,49 @@
+package keymonitor.common
+
+import kotlin.reflect.KProperty
+
+object CONFIGS {
+    /** The email address from which the app's communication is coming */
+    val EMAIL_FROM: String by Conf("Key Monitor <keymonitor@example.com")
+    /** The email domain, as used by the Mailgun API */
+    val EMAIL_DOMAIN: String by Conf()
+    /** Mailgun API key */
+    val MAILGUN_API_KEY: String by Conf()
+}
+
+/** A [delegate class](https://kotlinlang.org/docs/reference/delegated-properties.html)
+ * that lazy-loads config values from the environment.
+ *
+ * Usage:
+ *
+ *      object MY_CONFIGS {
+ *          var SETTING: String by Conf("default value")
+ *      }
+ *
+ * Now, when you access MY_CONFIGS.SETTING, the class will return the value of the environment
+ * variable SETTING. If that doesn't exist, it will return the default value.
+ * If *that* value wasn't provided, an exception is thrown, but only on access time.
+ *
+ * Fun fact: if this class is named Config, it doesn't seem to work: Kotlin claims an "unresolved reference."
+ *
+ * @constructor takes a default value to return if the property isn't available in the environment
+ * @throws RuntimeException at access-time if no value exists for property
+ */
+class Conf(val default: String? = null) {
+    private var value: String? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        if (value == null) {
+            // This property hasn't been set yet.
+            // Try to load it from the environment, or use the default.
+            value = System.getenv(property.name) ?: default
+        }
+
+        return value
+                ?: throw RuntimeException("missing config: ${property.name}")
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, newValue: String) {
+        value = newValue
+    }
+}
---
+++ src/test/kotlin/keymonitor/common/ConfigTest.kt
@@ -0,0 +1,47 @@
+package keymonitor.common
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+
+class ConfigTest : Spek({
+    describe("the Conf delegate class") {
+        on("the config exists in the environment") {
+            it("gets values from the environment") {
+                // This test depends on a environment variable that exists.
+                // PATH is a good bet for this, but there's a chance it doesn't exist.
+                if (System.getenv("PATH") != null) {
+                    val sampleConfig = object {
+                        var PATH: String by Conf()
+                    }
+
+                    assertEquals(System.getenv("PATH"), sampleConfig.PATH)
+                }
+            }
+        }
+
+        on("the config doesn't exist in the environment") {
+            it("throws an exception if no default is provided") {
+                val sampleConfig = object {
+                    var UNLIKELY_ENVIRONMENT_VARIABLE_NAME: String by Conf()
+                }
+
+                assertFailsWith(RuntimeException::class, {
+                    sampleConfig.UNLIKELY_ENVIRONMENT_VARIABLE_NAME
+                })
+            }
+
+            it("uses a default if one is provided") {
+                val sampleConfig = object {
+                    var UNLIKELY_ENVIRONMENT_VARIABLE_NAME: String by Conf("yay, defaults!")
+                }
+
+                assertEquals("yay, defaults!", sampleConfig.UNLIKELY_ENVIRONMENT_VARIABLE_NAME)
+            }
+        }
+    }
+})
