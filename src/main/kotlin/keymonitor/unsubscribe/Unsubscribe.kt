3790d93d449858fa5b00bb2d676bd8eeeb89c0bf
Nathan Malkin <nmalkin@cs.berkeley.edu>
2017-06-20 14:05:26 -0700

    Implement unsubscribe logic

--- src/main/kotlin/keymonitor/unsubscribe/Unsubscribe.kt
+++ src/main/kotlin/keymonitor/unsubscribe/Unsubscribe.kt
@@ -1,11 +1,29 @@
 package keymonitor.unsubscribe
 
+import keymonitor.database.EmailStatus
+import keymonitor.database.UserStatus
+import keymonitor.database.getEmail
+import keymonitor.database.save
+
 
 enum class UnsubscribeResult { SUCCESS, FAIL }
 
 /**
  * Processes an unsubscribe request for the given token
+ *
+ * To unsubscribe, the email identified by the token is marked as EmailStatus.UNSUBSCRIBED,
+ * and the associated user is marked as UserStatus.DEACTIVATED.
+ *
+ * @return FAIL if no email could be found with this token
  */
 fun processUnsubscribe(unsubscribeToken: String): UnsubscribeResult {
-    return UnsubscribeResult.FAIL
+    val email = getEmail(unsubscribeToken) ?: return UnsubscribeResult.FAIL
+
+    email.status = EmailStatus.UNSUBSCRIBED
+    email.save()
+
+    email.user.status = UserStatus.DEACTIVATED
+    email.user.save()
+
+    return UnsubscribeResult.SUCCESS
 }
