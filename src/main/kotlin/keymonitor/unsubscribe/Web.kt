ac42a2bf399244bab7e601411688679a0a8afaec
Nathan Malkin <nmalkin@cs.berkeley.edu>
2017-06-20 17:30:43 -0700

    Hook up unsubscribe logic to web application

--- src/main/kotlin/keymonitor/unsubscribe/Web.kt
+++ src/main/kotlin/keymonitor/unsubscribe/Web.kt
@@ -1,22 +1,53 @@
 package keymonitor.unsubscribe
 
-import org.jetbrains.ktor.jetty.*
-import org.jetbrains.ktor.routing.*
-import org.jetbrains.ktor.application.*
-import org.jetbrains.ktor.host.*
-import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.call
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.jetty.Jetty
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
 
-fun main(args: Array<String>) {
-    embeddedServer(Jetty, 8080) {
-        routing {
-            get("/") {
-                val token = call.parameters["t"]
-                if (token == null) {
-                    call.respondText("missing token")
-                } else {
-                    call.respondText("unsubscribe token: $token!")
+/** The unsubscribe web service */
+fun Application.main() {
+    routing {
+        get("/") {
+            val token = call.parameters["t"]
+            if (token == null) {
+                call.respondText("missing token")
+            } else {
+                try {
+                    val unsubscribe = processUnsubscribe(token)
+                    when (unsubscribe) {
+                        UnsubscribeResult.FAIL -> {
+                            call.response.status(HttpStatusCode.BadRequest)
+                            call.respondText("We're sorry, the link you clicked on is valid.")
+                        }
+                        UnsubscribeResult.SUCCESS -> {
+                            call.response.status(HttpStatusCode.OK)
+                            call.respondText("Thanks, we've processed your unsubscribe request.")
+                        }
+                    }
+                } catch (e: Exception) {
+                    call.response.status(HttpStatusCode.InternalServerError)
+                    call.respondText("We're sorry, something went wrong.")
                 }
             }
         }
-    }.start(wait = true)
+    }
+}
+
+/**
+ * Launch the unsubscribe web process in a Jetty container
+ *
+ * @param port the port to listen on
+ */
+fun launch(port: Int) {
+    embeddedServer(Jetty, port, module = Application::main)
+            .start(wait = true)
+}
+
+fun main(args: Array<String>) {
+    launch(8080)
 }
---
+++ src/test/kotlin/keymonitor/unsubscribe/WebTest.kt
@@ -0,0 +1,48 @@
+package keymonitor.unsubscribe
+
+import keymonitor.common.PhoneNumber
+import keymonitor.common.closeTestingDatabase
+import keymonitor.common.useTestingDatabase
+import keymonitor.database.addEmail
+import keymonitor.database.createUser
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.http.HttpMethod
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.testing.handleRequest
+import org.jetbrains.ktor.testing.withTestApplication
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    var testDB: File? = null
+
+    @BeforeEach fun setup() {
+        testDB = useTestingDatabase()
+    }
+
+    @Test fun testMissingToken() = withTestApplication(Application::main) {
+        with(handleRequest(HttpMethod.Get, "/")) {
+            assertEquals("missing token", response.content)
+        }
+    }
+
+    @Test fun testBadToken() = withTestApplication(Application::main) {
+        with(handleRequest(HttpMethod.Get, "/?t=badtoken")) {
+            assertEquals(HttpStatusCode.BadRequest, response.status())
+        }
+    }
+
+    @Test fun testWorkingToken() = withTestApplication(Application::main) {
+        val email = addEmail(createUser(PhoneNumber("+15105550123")), "test@example.com")
+        with(handleRequest(HttpMethod.Get, "/?t=${email.unsubscribeToken}")) {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
+    }
+
+    @AfterEach fun tearDown() {
+        closeTestingDatabase(testDB)
+    }
+}
